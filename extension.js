d93ae003551409e0f1ae91ea6e4566a1846f539f
hllvc <hllvc@icloud.com>
2022-03-22 09:33:30 +0100

    feature/hbox_member: updated _box member to _hbox

--- extension.js
+++ extension.js
@@ -19,7 +19,7 @@ function style() {
     monitorIndex < OsdWindowManager._osdWindows.length;
     monitorIndex++
   ) {
-    OsdWindowManager._osdWindows[monitorIndex]._box.add_style_class_name(
+    OsdWindowManager._osdWindows[monitorIndex]._hbox.add_style_class_name(
       "osd-transparency"
     );
   }
@@ -31,7 +31,7 @@ function unstyle() {
     monitorIndex < OsdWindowManager._osdWindows.length;
     monitorIndex++
   ) {
-    OsdWindowManager._osdWindows[monitorIndex]._box.remove_style_class_name(
+    OsdWindowManager._osdWindows[monitorIndex]._hbox.remove_style_class_name(
       "osd-transparency"
     );
   }
@@ -80,11 +80,11 @@ function enable() {
       let transparency = _settings.get_boolean("transparency");
       transparency ? style() : unstyle();
 
-      this._box.translation_x = (h_percent * monitor.width) / 100;
-      this._box.translation_y = (v_percent * monitor.height) / 100;
+      this._hbox.translation_x = (h_percent * monitor.width) / 100;
+      this._hbox.translation_y = (v_percent * monitor.height) / 100;
 
       this._icon.icon_size = (osd_size * monitor.height) / 100 / 2;
-      this._boxConstraint._minSize = (osd_size * monitor.height) / 100;
+      this._hboxConstraint._minSize = (osd_size * monitor.height) / 100;
 
       imports.ui.osdWindow.HIDE_TIMEOUT = hide_delay;
     }
@@ -98,7 +98,7 @@ function disable() {
   let arrayOSD = Main.osdWindowManager._osdWindows;
   for (let i = 0; i < arrayOSD.length; i++) {
     arrayOSD[i]._relayout();
-    arrayOSD[i]._box.translation_x = 0;
+    arrayOSD[i]._hbox.translation_x = 0;
     imports.ui.osdWindow.HIDE_TIMEOUT = 1500;
   }
 
